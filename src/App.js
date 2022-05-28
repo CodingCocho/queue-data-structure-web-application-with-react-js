a957bb7db687c2902189765e4b4b9b884cb8d45d
CodingCocho <raulujr@gmail.com>
2022-05-28 15:44:10 -0700

    Revert "Finalized application for deployment."
    This reverts commit cb97190ec7bdff7ad5fe84471c69d4eed7520186.

--- src/App.js
+++ src/App.js
@@ -1,77 +1,54 @@
 import './App.css';
-import PriorityQueue from './PriorityQueue'
+import Queue from './Queue'
 import { useState, useEffect } from "react";
 
 /*
-This React.js App will run the frontend of the Priority Queue data structure.
+This React.js App will run the frontend of the Queue data structure.
 */
 function App() {
   
   // Holds our useStates
-  // An useState for the Priority Queue to keep track of the data structure 
-  const [priorityQueue, setPriorityQueue] = useState(new PriorityQueue(5))
+  // An useState for the Queue to keep track of the data structure 
+  const [queue, setQueue] = useState(new Queue(5))
   // An useState for the pointer to keep track of when the page needs to render
-  const [pointer, setPointer] = useState(priorityQueue.getPointer())
-  // An useState for the type of Priority Queue
-  const [type, setType] = useState(priorityQueue.getType());
-  // An useState for the type button 
-  const [buttonMessage, setButtonMessage] = useState("Change to Min Heap")
- 
+  const [pointer, setPointer] = useState(queue.getPointer())
+
   // An useEffect the notifies the console of a render which is depended on the 
   // pointer changing
   useEffect(() => {
     console.log("render");
-  }, [pointer, type])
+  }, [pointer])
   
-  // Local function to set the new Priority Queue 
+  // Local function to set the new Queue 
   const newQueue = () =>
   {
-    setPriorityQueue(new PriorityQueue(5));
+    setQueue(new Queue(5));
   }
 
-  // Local function to enqueue an element into the Priority Queue
+  // Local function to enqueue an element into the Queue
   const enqueueQueue = () =>
   {
     // Prompt the user for the element
     const element = prompt("Enter an element to enqueue into the Queue", 0)
-    // Call the Priority Queue enqueue method
-    priorityQueue.enqueue(element);
+    // Call the Queue enqueue method
+    queue.enqueue(element);
     // Set the pointer in the useState to rerender
-    setPointer(priorityQueue.getPointer())
+    setPointer(queue.getPointer())
   }
 
-  // Local function to dequeue an element from the Priority Queue
+  // Local function to dequeue an element from the Queue
   const dequeueQueue = () =>
   {
-    // Call the Priority Queue dequeue method
-    priorityQueue.dequeue();
+    // Call the Queue dequeue method
+    queue.dequeue();
     // Set the pointer in the useState to rerender
-    setPointer(priorityQueue.getPointer())
+    setPointer(queue.getPointer())
   }
 
-  // Local function to peek the top element from the Priority Queue
   const peekQueue = () =>
   {
-    // Call the Priority Queue peek method
-    priorityQueue.peek();
-  }
-
-  //  Local function to change the type of Priority Queue
-  const changeQueueType = () =>
-  {
-    // Check type of Priority Queue
-    if(type === "max")
-    {
-      setButtonMessage("Change to Max Heap");
-      priorityQueue.changeType();
-      setType("min");
-    }
-    else
-    {
-      setButtonMessage("Change to Min Heap");
-      priorityQueue.changeType();
-      setType("max");
-    }
+    // Call the Queue peek method
+    queue.peek();
   }
 
   return (
@@ -79,13 +56,13 @@ function App() {
 
         <div className='heading-elements'>
           <p id='title'>Data Structure Demonstration</p>
-          <span id='data-structure-name'>Priority Queue</span>
+          <span id='data-structure-name'>Queue</span>
         </div>
 
         <div className='queue-container'>
           <div className='queue'>
             {/* Map out the Queue and create appropriate divs for elements */}
-            {priorityQueue.getQueue().map( (element) =>
+            {queue.getQueue().map( (element) =>
             {
               // Return empty div for undefined elements
               if(element === undefined)
@@ -99,8 +76,7 @@ function App() {
         </div>
         
         <div className='queue-functions-container'>
-          <button onClick={newQueue}>New Priority Queue</button>
-          <button onClick={changeQueueType}>{buttonMessage}</button>
+          <button onClick={newQueue}>New Queue</button>
           <button onClick={enqueueQueue}>Enqueue</button>
           <button onClick={dequeueQueue}>Dequeue</button>
           <button onClick={peekQueue}>Peek</button>
